1659cfb01382d0a7cfbdd1764a2d21946a8e37d4
Aleix Caminal i Mallol <aleixcam96@gmail.com>
2018-11-13 16:26:02 +0100

    create users

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 #!/usr/bin/env node
 
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const app = require('express')()
+const bodyParser = require('body-parser')
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const MongoClient = require('mongodb').MongoClient
 
-const { argv: [, , port = 9000] } = process
-server.listen(port);
-console.log('SERVER: listening on port ' + port)
+const { argv: [, , PORT = 9000] } = process
+const MONGO_URL = 'mongodb://localhost:27017'
+const MONGO_DBNAME = 'priviet'
+
+app.use(bodyParser.urlencoded({extended: false}))
+server.listen(PORT)
+console.log('SERVER: listening on port ' + PORT)
+const mongoClient = new MongoClient(MONGO_URL)
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/users', function (req, res) {
+    const user = {
+        email: req.body.email,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        password: req.body.password
+    }
+
+    mongoClient.connect(function(err) {
+        const db = mongoClient.db(MONGO_DBNAME)
+        const users = db.collection('users')
+        users.insertOne(user, function(err, res) {
+            mongoClient.close()
+        })
+    })
+
+    res.redirect('/')
+})
 
 io.on('connection', function (socket) {
     console.log('SERVER: new user connected')
-});
+})
